a027a1ee70eba262bdeef51ce145002d3f0ab0e5
Will Palmer <willpalmer@alertacall.com>
2024-10-27 16:36:25 +0000

    add tests init file

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='shared_utils',
-    version='0.2.1',
-    packages=find_packages(),
-    include_package_data=True,  # Ensures non-code files like test scripts are included
+    version='0.2.2',
+    packages=find_packages(include=['shared_utils', 'tests']),  # Explicitly include tests package
+    include_package_data=True,
     install_requires=[
         'python-dotenv',
         'SQLAlchemy>=2.0',
@@ -22,7 +22,7 @@ setup(
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
-            'test_shared_utils=tests.test:test_connections'  # Command to run test
+            'test_shared_utils=tests.test:test_connections'
         ]
     },
 )
